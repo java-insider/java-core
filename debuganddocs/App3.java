2f8f2ab031f0e4d8041beb52c8ca756c8cc9ba2b
Carlos Tosin <carlos.tosin@pixiongames.com>
2022-10-14 10:37:50 +0100

    @SuppressWarnings

---
+++ debuganddocs/App3.java
@@ -0,0 +1,11 @@
+package debuganddocs;
+
+@SuppressWarnings("unused")
+public class App3 {
+
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 1;
+        System.out.println(j);
+    }
+}
