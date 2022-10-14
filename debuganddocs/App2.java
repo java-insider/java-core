9f88dcf6cf2ad9a167b1301cae025a996c0cb1c4
Carlos Tosin <carlos.tosin@pixiongames.com>
2022-10-14 09:38:17 +0100

    Code

---
+++ debuganddocs/App2.java
@@ -0,0 +1,9 @@
+package debuganddocs;
+
+public class App2 {
+
+    public static void main(String[] args) {
+        System.getSecurityManager().checkPrintJobAccess();
+        Javadoc javadoc;
+    }
+}
